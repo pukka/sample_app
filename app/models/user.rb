fc5aee1ce45f0dd9eea2da00d4f914a963e113fa
FukazawaShun <breakdown547@gmail.com>
2015-03-09 16:11:12 +0900

    Add a index of database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: {maximum: 50}
   VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VAILD_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VAILD_EMAIL_REGEX },
+	      uniqueness: { case_sensitive: false }
 end
